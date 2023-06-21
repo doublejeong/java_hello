69901c8fc540e746fe7e227cb7af9d0dcf2efb49
doublejeong <chungjungsoo@gmail.com>
2023-06-21 23:40:34 +0900

    230621

---
+++ src/edu/global/one/Circle.java
@@ -0,0 +1,8 @@
+package edu.global.one;
+
+public class Circle {
+	public Circle() {
+		System.out.println("패기지가 edu.global.one");
+	}
+
+}
---
+++ src/edu/global/two/Circle.java
@@ -0,0 +1,8 @@
+package edu.global.two;
+
+public class Circle {
+	public Circle() {
+		System.out.println("패기지가 edu.global.two");
+	}
+	
+}
